2adf576bedaf0946b2bc0801fe5fce72ab2c8420
verdulife <verdu@live.com>
2026-08-08 01:46:22 +0200

    feat: integrar 13 proyectos de verdu en el marquee (imagen + enlace)

---
+++ src/data/projects.ts
@@ -0,0 +1,22 @@
+export interface Project {
+  id: string;
+  title: string;
+  url: string;
+  image: string;
+}
+
+export const projects: Project[] = [
+  { id: "kncelados-podcast", title: "Kncelados Podcast", url: "https://www.kncelados.com/", image: "/projects/kncelados-podcast.jpg" },
+  { id: "jardinería-la-medeterránia", title: "Jardinería La Medeterránia", url: "https://www.jardinerialamediterrania.com/", image: "/projects/jardinería-la-medeterránia.jpg" },
+  { id: "calandra-automovili", title: "Calandra Automovili", url: "https://www.calandraautomobili.com/", image: "/projects/calandra-automovili.jpg" },
+  { id: "ulavet-veterinaria", title: "Ulavet Veterinaria", url: "https://www.ulavet.com/", image: "/projects/ulavet-veterinaria.jpg" },
+  { id: "gaplogic", title: "Gaplogic", url: "https://testing.gaplogic.com/", image: "/projects/gaplogic.jpg" },
+  { id: "gaudio-art", title: "Gaudio Art", url: "https://www.gaudiobarcelona.com/", image: "/projects/gaudio-art.jpg" },
+  { id: "stopper-internacional", title: "Stopper Internacional", url: "https://www.stopperinternational.com/", image: "/projects/stopper-internacional.jpg" },
+  { id: "tattoo-killer", title: "Tattoo Killer", url: "https://www.tattookiller.es/", image: "/projects/tattoo-killer.jpg" },
+  { id: "sitgesgi", title: "Sitgesgi", url: "https://www.sitgesgi.com", image: "/projects/sitgesgi.jpg" },
+  { id: "simplemente-wear", title: "Simplemente Wear", url: "https://www.simplementewear.com", image: "/projects/simplemente-wear.jpg" },
+  { id: "sgl-vilanova", title: "SGL Vilanova", url: "https://www.sglvilanova.com", image: "/projects/sgl-vilanova.jpg" },
+  { id: "seastone", title: "Seastone", url: "https://www.seastone.es/", image: "/projects/seastone.jpg" },
+  { id: "mdi-sitges", title: "MDI Sitges", url: "https://mdisitges.com/", image: "/projects/mdi-sitges.jpg" },
+];
